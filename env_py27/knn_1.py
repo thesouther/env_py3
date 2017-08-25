13385d1dade80366fd4870bce5f814c62205f471
cc <1183864793@qq.com>
2017-08-25 14:49:30 +0800

    add env_py27

---
+++ env_py27/knn_1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 25 14:32:50 2017
+
+@author: cc
+"""
+
--- env_py3/test2.py
+++ env_py3/test2.py
@@ -7,5 +7,6 @@ def drawCurve(a,CurrentLevel):#a即前文中的系数a，CurrentLevel即Xn
     Time = [i for i in range(30)]
     plt.plot(Time, RegentLevel)
 for i in range(1,100):
-    drawCurve(1.5,i/100)
-plt.annotate('a = 1.5',xy = (5, 0.4),xytext = (6,0.9),arrowprops = dict(facecolor = 'black',shrink = 0.1))
+    drawCurve(4,i/100)
+plt.annotate('a = 6',xy = (5, 0.4),xytext = (6,0.9),arrowprops = dict(facecolor = 'black',shrink = 0.1))
+
